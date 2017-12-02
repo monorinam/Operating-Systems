104a2a10e875ce174ce951f8837ed948f7f7029b
Monorina M <monorinam@gmail.com>
2017-12-02 17:40:47 -0500

    writes 2 blocks

--- COMP310/Assignment3/supporting_files/little_test.c
+++ COMP310/Assignment3/supporting_files/little_test.c
@@ -11,18 +11,37 @@
 
 int main() {
     mksfs(1);
-    int f = sfs_fopen("some_name.txt");    
-    char my_data[] ="The quick brown fox jumps over the lazy dog Poor old Lucy lost her shoe";
-    char my_data2[]="I forgot the rest of this stupid poem Potatoes";
-    char out_data[1024];
-    sfs_fwrite(f, my_data, sizeof(my_data));
+    int f = sfs_fopen("some_name.txt");
+   // char my_data[] ="The quick brown fox jumps over the lazy dog Poor old Lucy lost her shoe";
+  //  char my_data2[]="I forgot the rest of this stupid poem Potatoes";
+    char mydata[2049];
+    for(int i = 0; i < 2049; i ++)
+    {
+        mydata[i] = (char) i;
+    }
+
+   // char out_data[1024];
+    //sfs_fwrite(f, my_data, sizeof(my_data));
+    //sfs_fseek(f, 0);
+   // sfs_fread(f, out_data, sizeof(out_data)+1);
+    //sfs_fwrite(f, my_data2, sizeof(my_data2));
+    //sfs_fseek(f, 0); 
+   // printf("%s\n", out_data);
+    //sfs_fread(f, out_data, sizeof(out_data)+1);
+   // printf("%s\n", out_data + sizeof(my_data));
+    printf("%s\n",mydata);
+    sfs_fwrite(f,mydata,sizeof(mydata));
     sfs_fseek(f, 0);
-    sfs_fread(f, out_data, sizeof(out_data)+1);
-    sfs_fwrite(f, my_data2, sizeof(my_data2));
-    sfs_fseek(f, 0); 
-    printf("%s\n", out_data);
-    sfs_fread(f, out_data, sizeof(out_data)+1);
-    printf("%s\n", out_data + sizeof(my_data));
+    char out_data2[2049];
+    sfs_fread(f, out_data2, sizeof(out_data2)+1);
+    printf("%s\n",out_data2);
+    for(int i=0;i<2049;i++)
+    {
+        if(mydata[i]!=out_data2[i])
+        {
+            printf("My data is %c and outdata is %c \n",mydata[i],out_data2[i]);
+        }
+    }
 
     sfs_fclose(f);
 }
